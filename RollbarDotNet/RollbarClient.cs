cf374ecaf8e5d7b85bf7c46b09e340f92d1ebf5d
Julien Binet <julien.binet@agilitic.fr>
2016-11-24 09:24:48 +0100

    - Capture de toutes les exceptions lors de l’envoie de la requête - Retourn un GUID vide si la réponse est vide

--- RollbarDotNet/RollbarClient.cs
+++ RollbarDotNet/RollbarClient.cs
@@ -1,41 +1,51 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace RollbarDotNet
 {
-	/// <summary>
-	/// Client for accessing the Rollbar API
-	/// </summary>
-	public class RollbarClient : IRollbarClient
-	{
-		public RollbarConfig Config { get; }
+    public class RollbarClient : IRollbarClient
+    {
+        public RollbarConfig Config { get; }
 
-		public RollbarClient(RollbarConfig config)
-		{
-			Config = config;
-		}
+        public RollbarClient(RollbarConfig config)
+        {
+            Config = config;
+        }
 
-		public async Task<Guid> PostItemAsync(Payload payload)
-		{
-			var stringResult = await SendPostAsync("item/", payload);
-			return ParseResponse(stringResult);
-		}
+        public async Task<Guid> PostItemAsync(Payload payload)
+        {
+            var stringResult = await SendPostAsync("item/", payload);
+            return ParseResponse(stringResult);
+        }
 
-		private static Guid ParseResponse(string stringResult)
-		{
-			var response = JsonConvert.DeserializeObject<RollbarResponse>(stringResult);
-			return Guid.Parse(response.Result.Uuid);
-		}
+        static Guid ParseResponse(string stringResult)
+        {
+            if (string.IsNullOrEmpty(stringResult))
+                return Guid.Empty;
 
-		private async Task<string> SendPostAsync<T>(string url, T payload)
-		{
-			var httpClient = new HttpClient();
-			httpClient.BaseAddress = (new Uri(Config.EndPoint));
+            var response = JsonConvert.DeserializeObject<RollbarResponse>(stringResult);
+            return Guid.Parse(response.Result.Uuid);
+        }
 
-			var result = await httpClient.PostAsync(url, new StringContent(JsonConvert.SerializeObject(payload)));
-			return await result.Content.ReadAsStringAsync();
-		}
-	}
+        async Task<string> SendPostAsync<T>(string url, T payload)
+        {
+            try
+            {
+                var httpClient = new HttpClient();
+                httpClient.BaseAddress = (new Uri(Config.EndPoint));
+
+                var result = await httpClient.PostAsync(url, new StringContent(JsonConvert.SerializeObject(payload)));
+                return await result.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("[RollbarDotNet] Unable to post report");
+                Debug.WriteLine(ex);
+                return string.Empty;
+            }
+        }
+    }
 }
